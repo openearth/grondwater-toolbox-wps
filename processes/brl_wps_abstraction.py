ec5ebfbb77fbfb63e68b0e8d3641dc7310064c4c
hendriksen <gerrit.hendriksen@deltares.nl>
2025-07-23 14:58:36 +0200

    added timeout for the pywps server

--- processes/brl_wps_abstraction.py
+++ processes/brl_wps_abstraction.py
@@ -44,6 +44,9 @@ from pywps.inout.inputs import ComplexInput
 from pywps.inout.outputs import ComplexOutput
 from pywps.app.Common import Metadata
 
+import pywps
+pywps.config.server.timeout = 360
+
 # other
 import json
 
